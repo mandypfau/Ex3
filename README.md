326cdd5a6f33a970010aede12cb0977e620aaea8
mandy pfau <mandypfau@knights.ucf.edu>
2023-01-30 12:46:40 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Ex3
+May the best of your today's be the worst of your tomorrow's.
---
+++ index.js
@@ -0,0 +1,4 @@
+let randomnumber = Math.floor(Math.random()*3)
+let quotes = ["You learn more from failure than from success. Don’t let it stop you","Don’t let yesterday take up too much of today","Coming together is a beginning. Keeping together is progress. Working together is success"]
+let quote = quotes [randomnumber]
+console.log(quote)
